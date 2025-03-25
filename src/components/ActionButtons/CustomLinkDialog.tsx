55be8b475cb9b38685096c037748914c47d60808
JB PORTALS <118927681+JBPORTALS@users.noreply.github.com>
2025-03-25 16:40:07 +0530

    Update environment variable reference to include HTTPS in CustomLinkDialog and ComputerMockup components

--- src/components/ActionButtons/CustomLinkDialog.tsx
+++ src/components/ActionButtons/CustomLinkDialog.tsx
@@ -39,7 +39,7 @@ export default function Component({ localLink }: { localLink: string }) {
   const baseUrl =
     process.env.NODE_ENV === "development"
       ? "http://localhost:3000"
-      : process.env.VERCEL_URL;
+      : `https://${process.env.VERCEL_URL}`;
 
   const fullUrl = `${baseUrl}/${customPath}`;
 
